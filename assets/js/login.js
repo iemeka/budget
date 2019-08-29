ba63d881a37191e78abe62d4ec0911d8ffe5ca6a
iemeka <mekstines@gmail.com>
2019-08-29 08:52:41 +0100

    initial commit

---
+++ assets/js/login.js
@@ -0,0 +1,15 @@
+let button = document.getElementById("login");
+let username = document.getElementById("name");
+let password = document.getElementById("pass");
+
+button.addEventListener("click", () => {
+    let request = new XMLHttpRequest()
+    request.open('POST','http://127.0.0.1:5000/login', true);
+    request.setRequestHeader('content-type', 'application/json');
+    request.onreadystatechange = function (){
+        if (request.readyState == 4 && request.status == 200){
+            console.log(request.responseText)
+        }
+    }
+    request.send(JSON.stringify({'username':username.value, 'password':password.value}));
+});
